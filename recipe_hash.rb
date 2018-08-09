8df3093d3a0c20bb7881bff3d36d9c7d6a32116d
frisaac <frisaac@stu.naperville203.org>
2018-08-09 16:43:25 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -14,7 +14,7 @@
 # All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
 
 #2. Write a method that simply returns (not puts) your perfect_10 hash
- PERFECT_10_RECIPE = {
+ perfect_10_recipe = {
     :almond_flour => "2 cups",
     :gluten_freewhole_oats => "1 cup",
     :kosher_salt => "1/2 tea spoon",
@@ -26,45 +26,45 @@
     :olive_oil => "1/2 cup",
     :agave => "1/4 cup"
  }
-def recipe_ingredients(PERFECT_10_RECIPE)
-  return PERFECT_10_RECIPE
+def recipe_ingredients(perfect_10_recipe)
+  return perfect_10_recipe
 end
 recipe_ingredients
 
 #2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
-def amount_of_chocolate_chips(PERFECT_10_RECIPE)
-  return PERFECT_10_RECIPE[mini_dark_chocolate_chips]
+def amount_of_chocolate_chips(perfect_10_recipe)
+  return perfect_10_recipe[mini_dark_chocolate_chips]
 end
 amount_of_chocolate_chips
 
 #3: Use `puts` to print out every ingredient and amount in the hash by iterating through the collection. You'll want to use the `each` method.
-def ingredients_and_amounts(PERFECT_10_RECIPE)
-  PERFECT_10_RECIPE.each do |x,y|
+def ingredients_and_amounts(perfect_10_recipe)
+  perfect_10_recipe.each do |x,y|
     puts "#{x}: #{y}"
   end
 end
 ingredients_and_amounts
 
 #4: Now use `puts` to print out just the ingredients without the amounts. You'll want to use the `each_key` method.
-def ingredients(PERFECT_10_RECIPE)
-  PERFECT_10_RECIPE.each do |x,y|
+def ingredients(perfect_10_recipe)
+  perfect_10_recipe.each do |x,y|
     puts "#{x}"
   end
 end
 ingredients
 
 #5: Now use `puts` to print out just the amounts without the ingredients. You'll want to use the `each_value` method.
-def amounts(PERFECT_10_RECIPE)
-  PERFECT_10_RECIPE.each do |x,y|
+def amounts(perfect_10_recipe)
+  perfect_10_recipe.each do |x,y|
     puts "#{y}"
   end
 end
 amounts
 
 #6: Return the number of ingredients in the `perfect_10_recipe` hash.
-def number_ingredients(PERFECT_10_RECIPE)
+def number_ingredients(perfect_10_recipe)
   count = 0
-  PERFECT_10_RECIPE.each do |x,y|
+  perfect_10_recipe.each do |x,y|
     count += 1
   end
   return count
